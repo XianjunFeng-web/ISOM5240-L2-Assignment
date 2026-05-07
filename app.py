f2b0cda250553d6ec9c6eadcde70bb3cd09a92e5
XianjunFeng-web <fengxianjun000@hotmail.com>
2026-05-07 15:03:16 +0800

    app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from PIL import Image
 import time
 
 # App title
-st.title("⭐ Star Storytelling Application ⭐ Star")
+st.title("⭐ Storytelling Application ⭐")
 
 # Write some text
 st.write("Welcome to storytelling app!")
-st.write("Sweetie，It is the wonderful story time~")
-st.write("Let's enjoy the story!")
+st.write("Sweetie，It is the wonderful story time~ Let's enjoy the story! ")
+
 
 # File uploader for image and audio
 uploaded_image = st.file_uploader("Upload an image",
