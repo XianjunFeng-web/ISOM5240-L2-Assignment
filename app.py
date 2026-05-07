65bba7ee9d9fc560e5b2d8079abb9d37e926733d
XianjunFeng-web <fengxianjun000@hotmail.com>
2026-05-07 17:17:15 +0800

    app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def img2text(url):
 # Main part
    
 # File uploader for image and audio
-st.Header("First, pick up an image and then start our story journey 🔐")
+st.header("First, pick up an image and then start our story journey 🔐")
+
 uploaded_image = st.file_uploader("upload image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
@@ -28,7 +29,7 @@ if uploaded_image is not None:
         image = Image.open(uploaded_image)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
-st.Header("Click to turn your Image into Audio Story")
+st.header("Click to turn your Image into Audio Story")
 
 # Button interaction
 if st.button("Click Me"):
